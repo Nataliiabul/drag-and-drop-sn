fc821189a9dbbdf1fc77e10d37bf18c89fec5cfd
Nataliia <Natasha.bulo4nikova@yandex.ru>
2023-01-28 21:08:49 +0300

    Add necessary basic files

---
+++ assets/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color mainColor= Color(0xFF283044);
+  static final Color secondColor = Color(0xFF78A1BB);
+}
---
+++ lib/game.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
