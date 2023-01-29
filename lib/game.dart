c4cdeae5caa5ae4c25bcdc50b96a06dba2e24257
Nataliia <Natasha.bulo4nikova@yandex.ru>
2023-01-29 15:12:11 +0300

    basic ui

--- lib/game.dart
+++ lib/game.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'package:drag_and_drop_social_networks/assets/colors.dart';
 import 'package:drag_and_drop_social_networks/models/social_model.dart';
-
 
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
@@ -29,7 +29,7 @@ class _GameScreenState extends State<GameScreen> {
       SocialItemModel(
           name: 'Twitter', value: 'Twitter', icon: FontAwesomeIcons.twitter),
       SocialItemModel(
-          name: 'YouTube', value: 'YouTube', icon: FontAwesomeIcons.youtube),
+          name: 'Facebook', value: 'Facebook', icon: FontAwesomeIcons.facebook),
       SocialItemModel(name: 'VK', value: 'VK', icon: FontAwesomeIcons.vk),
       SocialItemModel(
           name: 'Instagram',
@@ -44,13 +44,65 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.mainColor,
       appBar: AppBar(
-        title: const Text('Drag&Drop'),
+        backgroundColor: AppColors.mainColor,
+        title: const Text(
+          'Drag&Drop',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: SingleChildScrollView(
-          child: Column(
-        children: [],
-      )),
+        padding: const EdgeInsets.all(15),
+        // main column
+        child: Column(
+          children: [
+            // row with matching columns
+            Row(
+              children: [
+                // column with icons
+                Column(
+                  children: socialItems.map((socialItem) {
+                    return Container(
+                      margin: const EdgeInsets.all(8),
+                      child: Icon(
+                        socialItem.icon,
+                        color: AppColors.secondColor,
+                        size: 55,
+                      ),
+                    );
+                  }).toList(),
+                ),
+                Spacer(),
+                // column with titles
+                Column(
+                  children: socialItems2.map((socialItem) {
+                    return Container(
+                      color: AppColors.secondColor,
+                      height: 55,
+                      width: 110,
+                      alignment: Alignment.center,
+                      margin: const EdgeInsets.all(8),
+                      child: Text(
+                        socialItem.name,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
