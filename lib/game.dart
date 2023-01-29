d8a1c64098381e3db5453300129f6e3ab5af7f3f
Nataliia <Natasha.bulo4nikova@yandex.ru>
2023-01-29 14:49:20 +0300

    model class + structure

--- lib/game.dart
+++ lib/game.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:drag_and_drop_social_networks/models/social_model.dart';
+
 
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
@@ -9,8 +13,44 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  late List<SocialItemModel> socialItems;
+  late List<SocialItemModel> socialItems2;
+
+  @override
+  void initState() {
+    super.initState();
+    initGame();
+  }
+
+  initGame() {
+    socialItems = [
+      SocialItemModel(
+          name: 'Google', value: 'Google', icon: FontAwesomeIcons.google),
+      SocialItemModel(
+          name: 'Twitter', value: 'Twitter', icon: FontAwesomeIcons.twitter),
+      SocialItemModel(
+          name: 'YouTube', value: 'YouTube', icon: FontAwesomeIcons.youtube),
+      SocialItemModel(name: 'VK', value: 'VK', icon: FontAwesomeIcons.vk),
+      SocialItemModel(
+          name: 'Instagram',
+          value: 'Instagram',
+          icon: FontAwesomeIcons.instagram),
+    ];
+    socialItems2 = List<SocialItemModel>.from(socialItems);
+    socialItems.shuffle();
+    socialItems2.shuffle();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drag&Drop'),
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [],
+      )),
+    );
   }
 }
---
+++ lib/models/social_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SocialItemModel {
+  final String name;
+  final String value;
+  final IconData icon;
+
+  SocialItemModel({
+    required this.name,
+    required this.value,
+    required this.icon,
+  });
+}
